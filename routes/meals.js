d729765dbfdf40b2c3ce7421a61614d11d1ebee3
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-05 16:16:09 -0500

    fixed first 2 bugs

--- routes/meals.js
+++ routes/meals.js
@@ -11,34 +11,30 @@ router.get('/', function(req, res, next) {
     });
   });
 });
-router.get('/new', function(req, res, next) {
-  res.render('new')
-});
+// router.get('/new', function(req, res, next) {
+//   res.render('new')
+// });
 
-router.post('/', function(req, res, next) {
-  models.Meals.create({
-   meal_name: req.body.meal_name,
-   amount: req.body.amount,
-   restaurant_name: req.body.restaurant_name,
-   location: req.body.location,
-   review: req.body.review
- }).then(function() {
-    res.redirect('/meals')
- });
-  });
+// router.post('/', function(req, res, next) {
+//   models.Meals.create({
+//    meal_name: req.body.meal_name,
+//    amount: req.body.amount,
+//    restaurant_name: req.body.restaurant_name,
+//    location: req.body.location,
+//    review: req.body.review
+//  }).then(function() {
+//     res.redirect('/meals')
+//  });
+//   });
 
-router.get('/:id', function(req, res, next) {
-  models.Meals.findById(req.params.id).then(function(movie) {
-   res.render('mealId', {
-    meals: meal
-    amount: meal.amount,
-    restaurant_name: meal.restaurant_name
-    location: meal.location
-    review: meal.review
-   });
-  });
-});
-
-
-
-
+// router.get('/:id', function(req, res, next) {
+//   models.Meals.findById(req.params.id).then(function(movie) {
+//    res.render('mealId', {
+//     meals: meal
+//     amount: meal.amount,
+//     restaurant_name: meal.restaurant_name
+//     location: meal.location
+//     review: meal.review
+//    });
+//   });
+// });
