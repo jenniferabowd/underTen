37c59d6597300d62fbfd2c6738953625e07fefdf
Chiemeka Ihebom <emekavilla@gmail.com>
2017-02-06 23:02:10 -0500

    added edit and delete functions

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var meals = require('./routes/meals');
 var app = express();
 var dotenv = require('dotenv').config();
 
+
+app.use(methodOverride('_method'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
