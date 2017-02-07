193e3577f1b4be306448b20997350cc9fc544c2c
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-06 19:28:36 -0500

    changes made before pulling from master

--- routes/meals.js
+++ routes/meals.js
@@ -22,17 +22,17 @@ router.get('/', function(req, res, next) {
 //   res.render('new')
 // });
 
-// router.post('/', function(req, res, next) {
-//   models.Meals.create({
-//    meal_name: req.body.meal_name,
-//    amount: req.body.amount,
-//    restaurant_name: req.body.restaurant_name,
-//    location: req.body.location,
-//    review: req.body.review
-//  }).then(function() {
-//     res.redirect('/meals')
-//  });
-//   });
+router.post('/', function(req, res, next) {
+  models.Meals.create({
+   meal_name: req.body.meal_name,
+   amount: req.body.amount,
+   restaurant_name: req.body.restaurant_name,
+   location: req.body.location,
+   review: req.body.review
+ }).then(function() {
+    res.redirect('/meals')
+ });
+});
 
 // router.get('/:id', function(req, res, next) {
 
